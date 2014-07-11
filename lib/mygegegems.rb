7e19334223175ac7a65936f7583a7b97b1d4ee06
kyoendo <postagie@gmail.com>
2014-07-11 21:27:48 +0900

    implement Client class

--- lib/mygegegems.rb
+++ lib/mygegegems.rb
@@ -1,5 +1,41 @@
+require "yaml"
+require "gems"
+
 require "mygegegems/version"
 
 module Mygegegems
-  # Your code goes here...
+  class << self
+    def update
+      Client.new.update
+    end
+  end
+
+  class Client
+    def initialize(path=File.expand_path('~/.mygegegems.yaml'))
+      @path = path
+    end
+
+    def update
+      data[Date.today] = get
+      save(data, @path)
+    end
+
+    def data
+      @data ||= load(@path)
+    end
+
+    def get
+      Gems.gems.map { |gem| { gem['name'] => gem['downloads'] } }
+    end
+    
+    def load(path)
+      YAML.load_file(path)
+    rescue Errno::ENOENT
+      {}
+    end
+    
+    def save(obj, path)
+      File.open(path, 'w') { |f| YAML.dump(obj, f) }
+    end
+  end
 end
