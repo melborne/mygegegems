f7b87c3d5a650356c2e11c9b5e1df3285fd7a414
kyoendo <postagie@gmail.com>
2014-07-12 23:51:03 +0900

    add color to stat output

--- lib/mygegegems/cli.rb
+++ lib/mygegegems/cli.rb
@@ -23,6 +23,7 @@ module Mygegegems
       total, diff_total, num_of_gems = Stat.total(target)
       space1, space2 = [total, diff_total].map { |t| t.to_s.size }
       header = header(date, t_date, target)
+
       puts header
       puts "-" * header.size
       puts body(gems, diffs, space1, space2)
@@ -32,24 +33,24 @@ module Mygegegems
 
     no_tasks do
       def header(date1, date2, label)
-        "As of #{date1} (#{label}: #{date2})"
+        "As of #{date1} (#{label}: \e[33m#{date2}\e[0m)"
       end
 
       def body(gems, diffs, space1, space2)
         gems = gems.sort_by { |_, dl| -dl }
         gems.map do |name, dl|
           if diffs && (diff = diffs[name])
-            "%#{space1}d +%#{space2}d %s" % [dl, diffs[name], name]
+            "%#{space1}d \e[33m+%#{space2}d \e[32m%s\e[0m" % [dl, diffs[name], name]
           else
             none = "-".center(space2)
-            "%#{space1}d  #{none} %s" % [dl, name]
+            "%#{space1}d  \e[33m#{none} \e[32m%s\e[0m" % [dl, name]
           end
         end
       end
 
       def footer(total, diff_total, num_of_gems, space1, space2)
         if diff_total
-          "%#{space1}d +%#{space2}d %s gems" % [total, diff_total, num_of_gems]
+          "%#{space1}d \e[33m+%#{space2}d \e[32m%s gems\e[0m" % [total, diff_total, num_of_gems]
         else
           "%#{space1}d %s gems" % [total, num_of_gems]
         end
