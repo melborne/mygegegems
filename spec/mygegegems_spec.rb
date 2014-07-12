49a65193f9460c1bfef082783f6f877fe611e393
kyoendo <postagie@gmail.com>
2014-07-12 09:25:00 +0900

    add helpers

--- spec/mygegegems_spec.rb
+++ spec/mygegegems_spec.rb
@@ -4,8 +4,4 @@ describe Mygegegems do
   it 'has a version number' do
     expect(Mygegegems::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,12 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'mygegegems'
+
+module Helpers
+  def source_root
+    File.join(File.dirname(__FILE__), 'fixtures')
+  end
+end
+
+RSpec.configure do |c|
+  c.include Helpers
+end
